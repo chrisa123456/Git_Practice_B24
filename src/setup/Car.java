51a2194f41754c0b91cda215c402555d49c9b787
fuatty <vuralfuat@hotmail.com>
2021-10-03 13:19:41 -0700

    combine car

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,18 +2,18 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // bos
 
     // bos ..
 
     // bos...
-=======
+
     //this is our car
 
     // more text
 
     // more text
->>>>>>> james
+
 
 }
